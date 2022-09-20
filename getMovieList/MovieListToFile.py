1d1ba779d9395c5f02656db73d3e102a862c7ca8
maakun <katocha@yahoo.co.jp>
2022-09-20 22:05:45 +0900

    初期コミット

---
+++ getMovieList/MovieListToFile.py
@@ -0,0 +1,54 @@
+## Youtubeの動画リストをファイルに書き出すとともにターミナルにも出力する
+
+from functools import total_ordering
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+import csv
+import pprint
+from config import Config
+
+config = Config()
+
+# No module named 'apiclient'　エラー(YouTube Data API(v3)) 出た場合
+# https://qiita.com/SEI_Go/items/df354b639506d9a2cd82
+
+YOUTUBE_API_KEY = config.api_key
+
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+
+cId = ['UC56lh82cD7lUz2rg7deYdAw', # アイドル教室メインチャンネル
+       'UCJlJX_UnSegdENRPWtOFXLw', # アイドル教室サブチャンネル
+       'UCkSDNSZxvWCMC_mRhT2z-sQ', # アイドル教室musicチャンネル
+       'UCFCwO4zhO84JDUvs8QUDM-w', # アイドル教室旧2ndチャンネル
+       'UCVWpv6M08brx2VfEZC7uOsg', # あいじゅにラバー
+       'UCPKzKqWwVWR_ph46bV3ayTA' # パステルガレット
+       ]
+
+search_response = youtube.search().list(
+    part="snippet",
+    channelId= cId[0],  # 取得するチャンネルの要素番号を入れる
+    maxResults=200,
+    order="date",
+    publishedAfter="2022-09-01T00:00:00Z",
+    publishedBefore="2022-09-15T00:00:00Z"
+).execute()
+
+
+#titles = [item["snippet"]["title"] for item in search_response["items"]]
+
+total = 0
+disp = 0
+
+with open('Movielist.txt', 'a', encoding='utf-8', newline="", ) as file:
+    writer = csv.writer(file, delimiter=';')
+
+    for item in search_response["items"]:
+        total = total + 1
+        if item["id"]["kind"] == 'youtube#video':
+            writer.writerow([item["snippet"]["publishedAt"],  "https://www.youtube.com/watch?v=" + item["id"]["videoId"],  item["snippet"]["title"]])
+            print(item["snippet"]["publishedAt"], ";", "https://www.youtube.com/watch?v=" + item["id"]["videoId"], ";",  item["snippet"]["title"])
+            disp = disp + 1
+        else:
+            print(item["id"]["kind"])
+
+print(total, disp)
---
+++ getMovieList/playtimes.py
@@ -0,0 +1,40 @@
+# 各チャンネルの情報を出力する
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from config import Config
+
+config = Config()
+
+YOUTUBE_API_KEY = config.api_key
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+
+cId = ['UC56lh82cD7lUz2rg7deYdAw', # アイドル教室メインチャンネル
+       'UCJlJX_UnSegdENRPWtOFXLw', # アイドル教室サブチャンネル
+       'UCkSDNSZxvWCMC_mRhT2z-sQ', # アイドル教室musicチャンネル
+       'UCFCwO4zhO84JDUvs8QUDM-w', # アイドル教室旧2ndチャンネル
+       'UCVWpv6M08brx2VfEZC7uOsg', # あいじゅにラバー
+       'UCPKzKqWwVWR_ph46bV3ayTA'] # パステルガレット
+
+print(cId)
+
+search_response = youtube.channels().list(
+    part='snippet,statistics',
+    id=cId,
+).execute()
+
+total = 0
+
+for item in search_response["items"]:
+    print('チャンネル名:',item['snippet']['title'])
+    print('登録者数:',item['statistics']['subscriberCount'],'人')
+    print('投稿動画数:',item['statistics']['videoCount'],'本')
+    print('総再生数:',item['statistics']['viewCount'],'回')
+    total = total + int(item['statistics']['videoCount'])
+
+print(total)
+
+#    print('チャンネル名:',search_response['items'][0]['snippet']['title'])
+#    print('登録者数:',search_response['items'][0]['statistics']['subscriberCount'],'人')
+#    print('投稿動画数:',search_response['items'][0]['statistics']['videoCount'],'本')
+#    print('総再生数:',search_response['items'][0]['statistics']['viewCount'],'回')
